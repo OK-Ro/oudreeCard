c379e71177cbd8c5063b2eab6101cbf00a540df7
OK-Ro <o.robert1994@hotmail.com>
2024-11-03 01:22:55 +0100

    firts commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import { CartProvider } from "./context.js/CartContext";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Checkout from "./pages/CheckOut";
+import ItemCard from "./pages/ItemCard";
+import PaymentPage from "./pages/PayementPage";
+
+const Container = styled.div`
+  padding: 2rem 6rem;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+    width: 100%;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <CartProvider>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/item/:id" element={<ItemCard />} />
+            <Route path="/payment" element={<PaymentPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </Container>
+  );
+}
+
+export default App;
